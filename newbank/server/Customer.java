30d9669ae9c101d08c5ff918248a0df313348bf8
NDR008 <nadir.syedsammut@gmail.com>
2020-11-15 15:28:14 +0100

    partial password_check

--- newbank/server/Customer.java
+++ newbank/server/Customer.java
@@ -2,12 +2,16 @@ package newbank.server;
 
 import java.util.ArrayList;
 
+import static newbank.server.MD5.getMd5;
+
 public class Customer {
 	
 	private ArrayList<Account> accounts;
+	private String password; // store hashed pass
 	
-	public Customer() {
+	public Customer(String password) {
 		accounts = new ArrayList<>();
+		this.password = getMd5(password);
 	}
 	
 	public String accountsToString() {
@@ -21,4 +25,12 @@ public class Customer {
 	public void addAccount(Account account) {
 		accounts.add(account);		
 	}
+
+	/* Add CheckPassword Function */
+	public boolean checkPassword(String password) {
+		String md5Pass = getMd5(password);
+		if (password.equals(md5Pass)) { return true; }
+		else {return false;}
+	}
+
 }
